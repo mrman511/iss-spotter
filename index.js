4cd04e76889dd35eafbd4c9aa72888f45a27ea7b
Paul Bodner <bodner80@gmail.com>
2022-01-20 18:27:55 -0800

    added location request to iss-spotter

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const { fetchMyIP } = require('./iss');
+const { fetchMyCoordsByIP } = require("./iss");
 
 // fetchMyIP ((error, ip) => {
 //   if (error) {
@@ -6,4 +7,12 @@ const { fetchMyIP } = require('./iss');
 //     return
 //   };
 //   console.log('It worked! ', ip)
+// });
+
+// fetchMyCoordsByIP(('173.180.16.68'), (error, location) => {
+//   if (error) {
+//     console.log("IT did not work! ", error);
+//     return;
+//   }
+//   console.log("It worked ", location);
 // });
--- iss.js
+++ iss.js
@@ -12,15 +12,38 @@ const fetchMyIP = (callback) => {
       const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
       callback(Error(msg), null);
       return;
-    } else { callback( null, data["ip"]) }; 
-  })
+    } else {
+      callback(null, data["ip"]);
+    }
+  });
 };
 
 const fetchMyCoordsByIP = (ip, callback) => {
-
+  const IPtracking = `https://api.freegeoip.app/json/${ip}?apikey=0a73bd50-7a5b-11ec-bbc7-f701212dee0c`;
+  request(IPtracking, (error, response, body) => {
+    //console.log(body)
+    let data = JSON.parse(body);
+    const longitude = data['longitude'];
+    const latitude = data['latitude'];
+    const location = {
+      longitude,
+      latitude
+    };
+    if (error) {
+      callback(error, null);
+    }
+    if (response.statusCode === 404) {
+      const msg = `Status Code ${response.statusCode} when fetching Location. Response: ${body}`;
+      callback(Error(msg), null);
+      return;
+    } else {
+      callback(null, location);
+    }
+  });
 };
 
 //fetchMyIp();
+//fetchMyCoordsByIP('173.180.16.68')
 
 module.exports = {
   fetchMyIP,
