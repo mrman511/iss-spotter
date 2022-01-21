dfa00d3262621a4ff48aa30a292f10868362d0dc
Paul Bodner <bodner80@gmail.com>
2022-01-20 18:48:18 -0800

    added api functionality to get mock information about timing

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const { fetchMyIP } = require('./iss');
-const { fetchMyCoordsByIP } = require("./iss");
+const { fetchMyCoordsByIP, fetchOverHead } = require("./iss");
 
 // fetchMyIP ((error, ip) => {
 //   if (error) {
@@ -10,9 +10,17 @@ const { fetchMyCoordsByIP } = require("./iss");
 // });
 
 // fetchMyCoordsByIP(('173.180.16.68'), (error, location) => {
-//   if (error) {
-//     console.log("IT did not work! ", error);
-//     return;
-//   }
-//   console.log("It worked ", location);
+// if (error) {
+//   console.log("IT did not work! ", error);
+//   return;
+// }
+// console.log("It worked ", location);
 // });
+
+fetchOverHead(({ longitude: -122.9317, latitude: 49.2286 }), (error, response) => {
+  if (error) {
+    console.log("IT did not work! ", error);
+    return;
+  }
+  console.log("It worked ", response);
+});
--- iss.js
+++ iss.js
@@ -29,6 +29,7 @@ const fetchMyCoordsByIP = (ip, callback) => {
       longitude,
       latitude
     };
+   
     if (error) {
       callback(error, null);
     }
@@ -41,11 +42,32 @@ const fetchMyCoordsByIP = (ip, callback) => {
     }
   });
 };
+const fetchOverHead = (longLatObj, callback) => {
+  const APIurl = `https://iss-pass.herokuapp.com/json/?lat=${longLatObj.latitude}&lon=${longLatObj.longitude}`;
+
+  request(APIurl, (error, response, body) => {
+    const data = JSON.parse(body);
+    const timeData = data['response'];
+    if (error) {
+      callback(error, null);
+    }
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching Location. Response: ${body}`;
+      callback(Error(msg), null);
+      return;
+    } else {
+      callback(null, timeData);
+    }
+  });
+};
+
 
 //fetchMyIp();
 //fetchMyCoordsByIP('173.180.16.68')
+//fetchOverHead({ longitude: -122.9317, latitude: 49.2286 },)
 
 module.exports = {
   fetchMyIP,
   fetchMyCoordsByIP,
+  fetchOverHead,
 };
