3d75775df48193dd1c100bc6fff32aac3ae27e72
Paul Bodner <bodner80@gmail.com>
2022-01-20 19:08:43 -0800

    callbacks nested in index.js for functionality

--- index.js
+++ index.js
@@ -17,10 +17,38 @@ const { fetchMyCoordsByIP, fetchOverHead } = require("./iss");
 // console.log("It worked ", location);
 // });
 
-fetchOverHead(({ longitude: -122.9317, latitude: 49.2286 }), (error, response) => {
-  if (error) {
-    console.log("IT did not work! ", error);
-    return;
-  }
-  console.log("It worked ", response);
-});
+// fetchOverHead(({ longitude: -122.9317, latitude: 49.2286 }), (error, response) => {
+//   if (error) {
+//     console.log("IT did not work! ", error);
+//     return;
+//   }
+//   console.log("It worked ", response);
+// });
+
+const nextISSTimesForMyLocation = () => {
+  fetchMyIP((error, ip) => {
+    if (error) {
+      console.log("IP did not work! ", error);
+      return;
+    }
+    fetchMyCoordsByIP((ip), (error, location) => {
+      if (error) {
+        console.log("Location did not work! ", error);
+        return;
+      }
+      fetchOverHead((location), (error, response) => {
+        if (error) {
+          console.log("response did not work! ", error);
+          return;
+        }
+        for (let item of response) {
+        //console.log(item);
+          console.log(`Risetime is ${item['risetime']}, for ${item['duration']} seconds`);
+        }
+      });
+    });
+    
+  });
+};
+
+nextISSTimesForMyLocation();
